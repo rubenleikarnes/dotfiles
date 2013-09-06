8da13a94b85b1020c0f4672b75f1fd20c2f21bd6
rubenleikarnes <ruben@2110.no>
2013-09-07 01:16:16 +0200

    Added submodules and a install file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# This is a fork of https://github.com/davidfischer/dotfiles/blob/master/install.sh
+
+DOTFILES=~/Projects/dotfiles/
+
+#rm -f ~/.bashrc && ln -s $DOTFILES/bashrc ~/.bashrc
+#rm -f ~/.gitconfig && ln -s $DOTFILES/gitconfig ~/.gitconfig
+#rm -f ~/.editorconfig && ln -s $DOTFILES/editorconfig ~/.editorconfig
+#rm -f ~/.vimrc && ln -s $DOTFILES/vimrc ~/.vimrc
+#rm -rf ~/.vim && ln -s $DOTFILES/vim ~/.vim
+
+cd $DOTFILES && git submodule update --init
+cd $DOTFILES/vim/bundle/jedi-vim && git submodule update --init
