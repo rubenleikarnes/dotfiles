bd671790ba4fe9ce3cfc3e9a2bf7a0d75e95a8cd
rubenleikarnes <ruben@2110.no>
2013-09-22 18:23:41 +0200

    Update

--- install.sh
+++ install.sh
@@ -3,10 +3,28 @@
 
 DOTFILES=~/Projects/dotfiles/
 
-#rm -f ~/.bashrc && ln -s $DOTFILES/bashrc ~/.bashrc
-#rm -f ~/.gitconfig && ln -s $DOTFILES/gitconfig ~/.gitconfig
-#rm -f ~/.editorconfig && ln -s $DOTFILES/editorconfig ~/.editorconfig
-#rm -f ~/.vimrc && ln -s $DOTFILES/vimrc ~/.vimrc
-#rm -rf ~/.vim && ln -s $DOTFILES/vim ~/.vim
+
+# Notice title
+# Output: => variabel
+notice() { echo  "\033[1;32m=> $1\033[0m"; }
+
+# Error title
+# Output: => Error: variabel
+error() { echo "\033[1;31m=> Error: $1\033[0m"; }
 
 cd $DOTFILES && git submodule update --init
+
+cd $HOME
+
+rm -f ~/.gitconfig && ln -s $DOTFILES/.gitconfig ~/.gitconfig
+rm -f ~/.gitignore && ln -s $DOTFILES/.gitignore ~/.gitignore
+rm -f ~/.vimrc && ln -s $DOTFILES/_vim/.vimrc ~/.vimrc
+rm -rf ~/.vim && ln -s $DOTFILES/_vim/.vim ~/.vim
+rm -rf ~/.oh-my-zsh && ln -s $DOTFILES/_zsh/.oh-my-zsh ~/.oh-my-zsh
+rm -rf ~/.zprofile && ln -s $DOTFILES/_zsh/.zprofile ~/.zprofile
+rm -rf ~/.zshalias && ln -s $DOTFILES/_zsh/.zshalias ~/.zshalias
+rm -rf ~/.zshrc && ln -s $DOTFILES/_zsh/.zshrc ~/.zshrc
+mkdir ~/.oh-my-zsh/custom/themes && ln -s $DOTFILES/_zsh/pure/pure.zsh ~/.oh-my-zsh/custom/themes/pure.zsh-theme
+
+notice "Done"
+exec $SHELL -l
