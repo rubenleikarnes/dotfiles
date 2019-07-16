ee895e836ced40d129687e4aa6cd103b40cac11b
ruben <rubenleikarnes@users.noreply.github.com>
2019-07-16 23:31:17 +0200

    Update

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 # Credits to https://github.com/nicknisi/dotfiles/blob/master/install/link.sh
 
 DOTFILES=$HOME/dotfiles
+BACKUPDIR=~/.dotfiles-backup-$(date +'%Y%m%d-%H%M')
 
 # Green list checkmark
 # Output: ✔ variabel
@@ -18,13 +19,24 @@ e_list() { echo "  \033[1;31m✖ Error: $1\033[0m"; }
 
 
 
+echo "\nCreating backup folder"
+echo "=============================="
+y_list "$BACKUPDIR created"
+mkdir $BACKUPDIR
+
+
+
+
 echo "\nCreating symlinks"
 echo "=============================="
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
 	target="$HOME/.$( basename $file '.symlink' )"
 	if [ -e $target ]; then
-		y_list "~${target#$HOME} already exists... Skipping."
+		y_list "~${target#$HOME} already exists... backing up."
+		mv $target $BACKUPDIR
+		g_list "Creating symlink for $file"
+		ln -s $file $target
 	else
 		g_list "Creating symlink for $file"
 		ln -s $file $target
@@ -36,32 +48,40 @@ done
 echo "\nInstalling nvim" 
 echo "=============================="
 if [ ! -d $HOME/.config ]; then
-	g_list "Creating ~/.config"
+	g_list "Creating $HOME/.config"
+	mkdir -p $HOME/.config
+else 
+	y_list "~/.config/ already exists... backing up."
+	mv $HOME/.config/ $BACKUPDIR/.config
+	y_list "Creating $HOME/.config"
 	mkdir -p $HOME/.config
 fi
 
 if [ ! -d $HOME/.config/nvim ]; then
-	g_list "Creating symlink for nvim"
+	g_list "Creating symlink for $DOTFILES/nvim"
 	ln -s $DOTFILES/nvim $HOME/.config/nvim 
 else
-	y_list "~/.config/nvim already exists... Skipping."
+	y_list "$HOME/.config/nvim already exists... Skipping."
+	mv -r $HOME/.config/nvim $BACKUPDIR/.config/nvim
+	g_list "Creating symlink for $DOTFILES/nvim"
+	ln -s $DOTFILES/nvim $HOME/.config/nvim 
 fi
 
 
 
-echo "\nInstalling Sublime Text" 
-echo "=============================="
-if [ ! -d  $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages ]; then
-	g_list "Creating Sublimee Packages folder"
-	mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages
-fi
+# echo "\nInstalling Sublime Text" 
+# echo "=============================="
+# if [ ! -d  $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages ]; then
+# 	g_list "Creating Sublimee Packages folder"
+# 	mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages
+# fi
 
-if [ ! -d $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ]; then
-	g_list "Creating symlink for Sublime Text"
-	ln -s $DOTFILES/sublime/User $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User  
-else
-	y_list "Sublime Text User folder already exists... Skipping."
-fi
+# if [ ! -d $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ]; then
+# 	g_list "Creating symlink for Sublime Text"
+# 	ln -s $DOTFILES/sublime/User $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User  
+# else
+# 	y_list "Sublime Text User folder already exists... Skipping."
+# fi
 
 
 
