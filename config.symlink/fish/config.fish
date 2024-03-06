7e8a1129018ff7db9ee6a4dbff7e0bbd516a6f09
Ruben <ruben@2110.no>
2024-03-06 09:56:25 +0100

    switched to starship prompt

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -1,12 +1,25 @@
+# enables truecolor for fish shell
 set -gx COLORTERM truecolor
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# ensure git prompt updates without lag
 # https://github.com/pure-fish/pure/wiki/Async-git-Prompt
 set -g async_prompt_functions _pure_prompt_git
 
+# load asdf runtime version manager
+# https://asdf-vm.com/
 source /usr/local/opt/asdf/libexec/asdf.fish
 
+# load personal aliases
 source ~/.config/fish/alias.fish
+
+# execute starship prompt, config is located in ~/.config/starship.toml
+# https://starship.rs/
+starship init fish | source
+
+# because fish does not honor ~/.hushlogin
+# https://fishshell.com/docs/current/faq.html#how-do-i-change-the-greeting-message
+set -U fish_greeting
