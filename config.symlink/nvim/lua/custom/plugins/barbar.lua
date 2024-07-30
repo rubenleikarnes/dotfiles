ecf262320900fe1e6f48847765e979cf3d24c381
ruben <ruben.leikarnes@gmail.com>
2024-07-30 14:25:39 +0200

    removed icons

--- config.symlink/nvim/lua/custom/plugins/barbar.lua
+++ config.symlink/nvim/lua/custom/plugins/barbar.lua
@@ -2,15 +2,22 @@ return {
   -- barbar.nvim is a tabline plugin with re-orderable, auto-sizing, clickable tabs, icons, nice highlighting, sort-by commands and a magic jump-to-buffer mode. Plus the tab names are made unique when two filenames match.
   'romgrk/barbar.nvim',
   dependencies = {
-    'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
     -- animation = true,
     -- insert_at_start = true,
     -- …etc.
+    icons = {
+      filetype = {
+        enabled = false,
+      },
+    },
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
