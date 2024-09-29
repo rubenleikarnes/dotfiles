384810230a48e9e4997043137e9f87fbf211f889
Ruben <ruben@2110.no>
2024-09-29 22:20:00 +0200

    Show hidden files

--- config.symlink/nvim/lua/custom/plugins/neotree.lua
+++ config.symlink/nvim/lua/custom/plugins/neotree.lua
@@ -10,6 +10,22 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- add extension names you want to explicitly exclude
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+          },
+      },
+    }
   end,
 }
