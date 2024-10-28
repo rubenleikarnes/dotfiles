035201c9ff01fcdeb4d1e78d6d0a6037c9bb29c5
Ruben <ruben@2110.no>
2024-10-28 10:46:04 +0100

    kubectl config

--- config.symlink/fish/alias.fish
+++ config.symlink/fish/alias.fish
@@ -2,3 +2,7 @@ alias dot="cd ~/dotfiles"
 alias dt="cd ~/Desktop"
 alias gp="git push"
 alias v="nvim"
+
+alias kubectl-configs="kubectl config get-contexts"
+alias kubectl-tfc="kubectl config use-context obc-app"
+alias kubectl-tfc-monitoring="kubectl config use-context obc-monitor"
