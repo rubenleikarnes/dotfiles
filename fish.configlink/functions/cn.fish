e4ca9f4f16a87f9baa4857d3217a7ae0f5a0d93c
rubenleikarnes <ruben@2110.no>
2025-06-19 10:05:48 +0200

    cn: addd completion to command

--- fish.configlink/functions/cn.fish
+++ fish.configlink/functions/cn.fish
@@ -5,3 +5,15 @@ function cn --argument-names 'folder'
     cd ~/code/nep-gpe/
   end
 end
+
+function __cn_complete
+    set arg (commandline -ct)
+    set saved_pwd $PWD
+
+    builtin cd ~/code/nep-gpe
+    and complete -C "cd $arg"
+
+    builtin cd $saved_pwd
+end
+
+complete --command cn --arguments '(__cn_complete)'
