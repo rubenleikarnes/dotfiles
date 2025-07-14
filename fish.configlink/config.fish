a1e76b039e3632347414a83073ee2e0fa2eef964
rubenleikarnes <ruben@2110.no>
2025-07-14 12:56:17 +0200

    Fix helix and whitespace

--- fish.configlink/config.fish
+++ fish.configlink/config.fish
@@ -13,28 +13,23 @@ set -x PATH $PATH /opt/homebrew/bin
 # enables truecolor for fish shell
 set -gx COLORTERM truecolor
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # ensure git prompt updates without lag
 # https://github.com/pure-fish/pure/wiki/Async-git-Prompt
 set -g async_prompt_functions _pure_prompt_git
 
 # set default editor
-if grep -qi arch /etc/os-release
+set -gx EDITOR hx
+
+# for some reason the helix command is "helix" instead of "hx" on arch...
+set uname (uname)
+if test "$uname" = "Linux" -a -f /etc/os-release
+  if grep -qi arch /etc/os-release
     set -gx EDITOR helix
-else
-    set -gx EDITOR hx
+  end
 end
+
 # load personal aliases
 source ~/.config/fish/alias.fish
-
-# execute starship prompt, config is located in ~/.config/starship.toml
-# https://starship.rs/
-if command -v starship >/dev/null
-    starship init fish | source
-end
 
 # because fish does not honor ~/.hushlogin
 # https://fishshell.com/docs/current/faq.html#how-do-i-change-the-greeting-message
@@ -47,17 +42,9 @@ set -x PATH $PATH $GOPATH/bin
 # add laravel valet to path
 set -x PATH $PATH $HOME/.composer/vendor/bin
 
-# load fzf
-if command -v fzf >/dev/null
-    fzf --fish | source
-end
-
 # kubectl plugin manager https://krew.sigs.k8s.io/docs/user-guide/quickstart/
 set -gx PATH $PATH $HOME/.krew/bin
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
-
-# Ruby gem paths in home to ease permissions
-#set -x GEM_HOME $HOME/.gem
 
 # asdf paths
 # for time being asdf has been installed manually and just placed in ~/.asdf, waiting for nix to update their package
@@ -68,7 +55,21 @@ set -x PATH $ASDF_DATA_DIR/shims $PATH
 set -x PATH $PATH /opt/homebrew/opt/openjdk/bin
 set -gx JAVA_HOME /opt/homebrew/opt/openjdk
 
-# zoxide, and replace cd
-if command -v zoxide >/dev/null
+# Commands to run in interactive sessions can go here
+if status is-interactive
+  # execute starship prompt, config is located in ~/.config/starship.toml
+  # https://starship.rs/
+  if command -v starship >/dev/null
+      starship init fish | source
+  end
+
+  # load fzf
+  if command -v fzf >/dev/null
+      fzf --fish | source
+  end
+
+  # zoxide, and replace cd
+  if command -v zoxide >/dev/null
     zoxide init fish --cmd cd | source
+  end
 end
