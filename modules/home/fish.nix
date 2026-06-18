9eb9fe69d51d5f5ca40e637f1a20e30150bb9090
rubenleikarnes <ruben@2110.no>
2026-06-18 13:51:54 +0200

    Add comment explaining mkOutOfStoreSymlink vs xdg.configFile

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
+  # mkOutOfStoreSymlink keeps the target writable (fish writes fish_variables).
+  # xdg.configFile would copy into the read-only nix store and break this.
   home.file.".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/legacy/fish";
 }
